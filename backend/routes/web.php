da1b195ae4d37d9856ecd0f5987f64b26d7021d8
Ambujkumar909 <ambukr8@gmail.com>
2025-12-31 18:17:42 +0530

    testing

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// 🔍 RAW DB CONNECTION TEST
+Route::get('/raw-db-test', function () {
+    $start = microtime(true);
+    DB::connection()->getPdo();
+    return round(microtime(true) - $start, 2);
 });
